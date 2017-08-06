00f88bf3e7bf3757d12ed2411da92a8c8d07a184
Oren Fix <orenfix@gmail.com>
2017-08-06 15:06:54 -0700

    Complete game + Game Over text

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -34,13 +34,15 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
+	$game.data('flippedCards',[]);
+	$game.data('gameFlippedCards',[]);
 	cardColors = [
 		'hsl(25,85%,65%)',
 		'hsl(55,85%,65%)',
 		'hsl(90,85%,65%)',
 		'hsl(160,85%,65%)',
 		'hsl(220,85%,65%)',
-		'hsl(265,,85%,65%)',
+		'hsl(265,85%,65%)',
 		'hsl(310,85%,65%)',
 		'hsl(360,85%,65%)'
 	];
@@ -51,6 +53,9 @@ MatchGame.renderCards = function(cardValues, $game) {
 		$card.data('flipped',false);
 		$card.data('color',cardColors[cardValues[x]-1]);
 		$game.append($card);
+		$card.click(function() {
+			MatchGame.flipCard($(this),$game);
+		});
 	}
 };
 
@@ -60,5 +65,48 @@ MatchGame.renderCards = function(cardValues, $game) {
  */
 
 MatchGame.flipCard = function($card, $game) {
+	var $flippedCards = $game.data('flippedCards');
+	var $gameFlippedCards = $game.data('gameFlippedCards');
 
+	if ( $card.data('flipped') == true ) {
+		return;
+	} else {
+		$card.css('background-color',$card.data('color'));
+		$card.append($card.data('value'));
+		$card.data('flipped',true);
+		$flippedCards.push($card);
+	}
+
+	if ( $flippedCards.length == 2 ) {
+		if ( $flippedCards[0].data('value') == $flippedCards[1].data('value') ) {
+			$flippedCards[0].css('background-color','rgb(153,153,153)').css('color','rgb(204,204,204)');
+			$flippedCards[1].css('background-color','rgb(153,153,153)').css('color','rgb(204,204,204)');
+			$flippedCards.length = 0;
+			$gameFlippedCards.push($card[0],$card[1]);
+			if ($gameFlippedCards.length == 16 ) {
+				MatchGame.gameOver();
+			}
+		} else {
+			setTimeout(function() {
+				$flippedCards[0].html('').css('background-color','rgb(32,64,86)').data('flipped',false);
+				$flippedCards[1].html('').css('background-color','rgb(32,64,86)').data('flipped',false);
+				$flippedCards.length = 0;
+			},500);
+		}
+	} else return;
 };
+
+MatchGame.gameOver = function() {
+	$('#game').addClass('gameOver');
+};
+
+
+
+
+
+
+
+
+
+
+
